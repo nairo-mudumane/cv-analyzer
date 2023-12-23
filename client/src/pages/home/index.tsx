eec28bef4fcf92730b1fbc17e20f51d0db256504
nairoMudumane <nazirmudumane@gmail.com>
2023-12-22 21:36:59 -0800

    update: home page

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,3 +1,22 @@
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
 export function Home() {
-  return <div>Home page</div>;
+  return (
+    <main className="w-screen h-screen flex flex-col gap-4 items-center justify-center">
+      <h1 className="text-primary font-bold text-3xl uppercase">
+        Cv 2Business
+      </h1>
+
+      <div className="flex gap-4 mt-6">
+        <Link to="/jobs">
+          <Button variant="contained">View Jobs</Button>
+        </Link>
+
+        <Link to="/upload">
+          <Button variant="outlined">Upload Resume</Button>
+        </Link>
+      </div>
+    </main>
+  );
 }
